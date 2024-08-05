f2b5a201c7d6b07394c1ee084a0ca76685f3f0b1
Tom Renard <tomrenard69@gmail.com>
2024-08-05 18:00:31 +0000

    Update file main.tf

--- main.tf
+++ main.tf
@@ -126,3 +126,39 @@ output "database_name" {
   description = "The name of the database"
   value       = aws_db_instance.rds_mysql.db_name
 }
+
+data "aws_vpc" "target_peering_vpc" {
+  id = "vpc-0f4d614772ca8d3f0"
+}
+
+resource "aws_vpc_peering_connection" "vpc_gitlab_runner" {
+  peer_vpc_id   = data.aws_vpc.target_peering_vpc.id
+  vpc_id        = var.vpc_id
+
+  tags = {
+    Side = "Requester"
+  }
+}
+
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+  auto_accept               = true
+
+  tags = {
+    Side = "Accepter"
+  }
+}
+
+# Create a route for the second VPC's CIDR block in the first VPC's route table
+resource "aws_route" "main_vpc_route_to_k8s" {
+  route_table_id            = "rtb-0cb9914b16f6618ed"
+  destination_cidr_block    = "192.168.0.0/16" # Replace with the other VPC's CIDR block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+}
+
+# Create a route for the first VPC's CIDR block in the second VPC's route table
+resource "aws_route" "vpc2_route" {
+  route_table_id            = data.aws_vpc.target_peering_vpc.id.main_route_table_id
+  destination_cidr_block    = "172.31.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_gitlab_runner.id
+}
