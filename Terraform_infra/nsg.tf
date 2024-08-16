ca87cf9d7c9f6296e2db43fb721feedeaf1f2b4d
Tom Renard <tomrenard69@gmail.com>
2024-08-16 18:38:19 +0000

    Update 3 files
    - /Terraform_infra/main.tf
    - /Terraform_infra/main.backup
    - /Terraform_infra/nsg.tf

--- Terraform_infra/nsg.tf
+++ Terraform_infra/nsg.tf
@@ -1,5 +1,28 @@
-resource "aws_security_group" "kubernetes_controlplane" {
-  name        = "ctlplane_sg"
-  description = "Allow SSH and necessary ports for k8s cluster - ctlplane"
-  vpc_id      = aws_vpc.main_vpc.id 
+# Security group for the RDS instance
+resource "aws_security_group" "rds_sg" {
+  description = "SG allowing 3306 for MySQL from nodes and runner"
+  vpc_id = data.aws_vpc.eks_vpc.id
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_ipv4         = "192.168.0.0/16"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_mysql_gitlabrunner" {
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_ipv4         = "172.31.32.99/32"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_out" {
+  security_group_id = aws_security_group.rds_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
