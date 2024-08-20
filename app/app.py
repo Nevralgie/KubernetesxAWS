c7d576a3b80f05b317e2a98aa6a0489030642627
Tom Renard <tomrenard69@gmail.com>
2024-08-20 21:48:11 +0000

    Update 5 files
    - /app/templates/404.html
    - /app/templates/500.html
    - /app/app.py
    - /K8s/manifests/stock_analysis_app/deployment_stock.yml
    - /.gitlab-ci.yml

--- app/app.py
+++ app/app.py
@@ -151,4 +151,4 @@ def handle_exception(e):
     return render_template('error.html'), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=false)
